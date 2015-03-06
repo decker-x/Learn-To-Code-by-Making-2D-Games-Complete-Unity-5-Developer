b012fd3e74036651b1e403e57a4f3e349265e0d2
hiperion <hiperionr3@telefonica.net>
2015-03-07 00:20:31 +0100

    Section 02 - 016 - Printing Text To The Console

---
+++ Number wizard console/Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizards : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to Number Wizard!");
+		print ("Pic a number in your head but don't tell me!");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
