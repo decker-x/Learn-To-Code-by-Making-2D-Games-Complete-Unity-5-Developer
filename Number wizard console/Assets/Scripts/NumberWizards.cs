56b68a1bdad5f1d15d5a13f077e1c427dc1cc8b1
hiperion <hiperionr3@telefonica.net>
2015-03-07 23:59:07 +0100

    Section 02 - 018 - Responding to key presses

--- Number wizard console/Assets/Scripts/NumberWizards.cs
+++ Number wizard console/Assets/Scripts/NumberWizards.cs
@@ -1,22 +1,31 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿		using UnityEngine;
+		using System.Collections;
 
-public class NumberWizards : MonoBehaviour {
+		public class NumberWizards : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		print ("Welcome to Number Wizard!");
-		print ("Pic a number in your head but don't tell me!");
-	
-		int max = 1000;
-		int min = 1;
+			// Use this for initialization
+			void Start () {
+				print ("Welcome to Number Wizard!");
+				print ("Pic a number in your head but don't tell me!");
+			
+				int max = 1000;
+				int min = 1;
 
-		print ("the highest number you can pick is" + max);
-		print ("the lowest number you can pick is" + min);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
+				print ("the highest number you can pick is" + max);
+				print ("the lowest number you can pick is" + min);
+
+				print ("is the number higher or lower than 500?" );
+				print ("Up = Higher, down = lower, return = equal");
+			}
+			
+			// Update is called once per frame
+			void Update () {
+					if (Input.GetKeyDown (KeyCode.UpArrow)) {
+							print ("Up arrow pressed");
+					}
+					if (Input.GetKeyDown (KeyCode.DownArrow)) {
+							print ("Down arrow pressed");
+					}
+			}
+
+		}
