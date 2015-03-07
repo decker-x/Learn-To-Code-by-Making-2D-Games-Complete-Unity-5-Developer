cf99fafe2e8599158d9a323148ec13280a1c0835
hiperion <hiperionr3@telefonica.net>
2015-03-07 23:35:03 +0100

    Section 02 - 017 - Using variables - Variables for things that change

--- Number wizard console/Assets/Scripts/NumberWizards.cs
+++ Number wizard console/Assets/Scripts/NumberWizards.cs
@@ -8,6 +8,11 @@ public class NumberWizards : MonoBehaviour {
 		print ("Welcome to Number Wizard!");
 		print ("Pic a number in your head but don't tell me!");
 	
+		int max = 1000;
+		int min = 1;
+
+		print ("the highest number you can pick is" + max);
+		print ("the lowest number you can pick is" + min);
 	}
 	
 	// Update is called once per frame
