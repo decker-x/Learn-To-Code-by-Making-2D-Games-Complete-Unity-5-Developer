0343c525728e7d4129e92562cafe04e4ed898028
hiperion <hiperionr3@telefonica.net>
2015-03-08 00:19:40 +0100

    Section 02 - 019 - Using If to make choices

--- Number wizard console/Assets/Scripts/NumberWizards.cs
+++ Number wizard console/Assets/Scripts/NumberWizards.cs
@@ -1,31 +1,31 @@
-﻿		using UnityEngine;
-		using System.Collections;
+﻿				using UnityEngine;
+				using System.Collections;
 
-		public class NumberWizards : MonoBehaviour {
+				public class NumberWizards : MonoBehaviour {
 
-			// Use this for initialization
-			void Start () {
-				print ("Welcome to Number Wizard!");
-				print ("Pic a number in your head but don't tell me!");
-			
-				int max = 1000;
-				int min = 1;
+					// Use this for initialization
+					void Start () {
+						print ("Welcome to Number Wizard!");
+						print ("Pic a number in your head but don't tell me!");
+					
+						int max = 1000;
+						int min = 1;
 
-				print ("the highest number you can pick is" + max);
-				print ("the lowest number you can pick is" + min);
+						print ("the highest number you can pick is" + max);
+						print ("the lowest number you can pick is" + min);
 
-				print ("is the number higher or lower than 500?" );
-				print ("Up = Higher, down = lower, return = equal");
-			}
-			
-			// Update is called once per frame
-			void Update () {
-					if (Input.GetKeyDown (KeyCode.UpArrow)) {
-							print ("Up arrow pressed");
+						print ("is the number higher or lower than 500?" );
+						print ("Up = Higher, down = lower, return = equal");
 					}
-					if (Input.GetKeyDown (KeyCode.DownArrow)) {
-							print ("Down arrow pressed");
+					
+					// Update is called once per frame
+					void Update () {
+							if (Input.GetKeyDown (KeyCode.UpArrow)) {
+									print ("Up arrow pressed");
+							}	else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+									print ("Down arrow pressed");
+							}	else if (Input.GetKeyDown (KeyCode.Return)) {
+									print ("I won");
 					}
-			}
-
-		}
+				}
+}
