dd30555fc73ab6e673c2b381f6d1f516d17ad202
Andrés Cerezo <andrescerezo@telefonica.net>
2015-03-10 08:43:56 +0100

    Error in Scope and context of variables
    Error with max and min variables (repeated local and global).

--- Number wizard console/Assets/Scripts/NumberWizards.cs
+++ Number wizard console/Assets/Scripts/NumberWizards.cs
@@ -14,9 +14,6 @@ public class NumberWizards : MonoBehaviour
 				print ("Welcome to Number Wizard!");
 				print ("Pic a number in your head but don't tell me!");
 					
-				int max = 1000;
-				int min = 1;
-
 				print ("the highest number you can pick is" + max);
 				print ("the lowest number you can pick is" + min);
 
