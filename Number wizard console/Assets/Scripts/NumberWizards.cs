0ee46e499292778d93c9b050f9bfc2051d017273
hiperion <hiperionr3@telefonica.net>
2015-03-10 23:30:20 +0100

    Section 02 - 021 - Simplifying by creating functions Section 02 - 022 - Completing number wizard

--- Number wizard console/Assets/Scripts/NumberWizards.cs
+++ Number wizard console/Assets/Scripts/NumberWizards.cs
@@ -3,37 +3,59 @@ using System.Collections;
 
 public class NumberWizards : MonoBehaviour
 {
-
-		// Use this for initialization
-		int max = 1000;
-		int min = 1;
-		int guess = 500;
-
-		void Start ()
-		{
-				print ("Welcome to Number Wizard!");
-				print ("Pic a number in your head but don't tell me!");
-					
-				print ("the highest number you can pick is" + max);
-				print ("the lowest number you can pick is" + min);
-
-				print ("is the number higher or lower than 500?");
-				print ("Up = Higher, down = lower, return = equal");
+	
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+	
+	void Start ()
+	{
+		max = 1000;
+		min = 1;
+		guess = 500;
+		
+		max = max + 1;
+		
+		StartGame ();
+	}
+	
+	void StartGame ()
+	{
+		
+		print ("===========================");
+		print ("Welcome to Number Wizard!");
+		print ("Pic a number in your head but don't tell me!");
+		
+		
+		print ("the highest number you can pick is" + max);
+		print ("the lowest number you can pick is" + min);
+		
+		print ("is the number higher or lower than 500?");
+		print ("Up = Higher, down = lower, return = equal");
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			min = guess;
+			NextGuess ();
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			max = guess;
+			NextGuess ();
+		} else if (Input.GetKeyDown (KeyCode.Return)) {
+			print ("I won");
+			StartGame ();
 		}
-					
-		// Update is called once per frame
-		void Update ()
-		{
-				if (Input.GetKeyDown (KeyCode.UpArrow)) {
-						min = guess;
-						guess = (max + min) / 2;
-						print ("Higher or lower than " + guess);
-				} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
-						max = guess;
-						guess = (max + min) / 2;
-						print ("Higher or lower than " + guess);
-				} else if (Input.GetKeyDown (KeyCode.Return)) {
-						print ("I won");
-				}
-		}
+	}
+	
+	void NextGuess ()
+	{
+		guess = (max + min) / 2;
+		print ("Higher or lower than " + guess);
+		print ("Up = Higher, down = lower, return = equal");
+	}
+	
 }
