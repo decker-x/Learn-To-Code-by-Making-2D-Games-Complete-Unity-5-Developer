6b4fefc5aa23b67f81f09dca90400b557a535388
hiperion <hiperionr3@telefonica.net>
2015-04-08 00:29:35 +0200

    Section 04 - 048 - How to Load Scenes and Quit

--- Number wizard ui/Assets/Scripts/LevelManager.cs
+++ Number wizard ui/Assets/Scripts/LevelManager.cs
@@ -7,11 +7,13 @@ public class LevelManager : MonoBehaviour
 	public void LoadLevel (string name)
 	{
 		Debug.Log ("New level load:" + name);
+		Application.LoadLevel (name);
 	}
 
 	public void QuitRequest ()
 	{
 		Debug.Log ("Quit requested");
+		Application.Quit ();
 	}
 
 }
