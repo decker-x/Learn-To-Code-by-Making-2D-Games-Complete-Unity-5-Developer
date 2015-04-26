964e66848c633cbf11d907d2f34fdd2ac96f36fc
hiperion <hiperionr3@telefonica.net>
2015-04-26 22:04:06 +0200

    Section 05 - 069 - Colliders Triggers and collisions in unity.

---
+++ Block breaker/Assets/Scripts/LooseColider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LooseColider : MonoBehaviour
+{
+
+	void OnTrigger2D (Collider2D trigger)
+	{
+		print ("Trigger");
+	}
+
+	void OnCollisionEnter2D (Collision2D	 collision)
+	{
+		print ("Collision");
+	}
+}
