98db980960a8fc7f0372bea5351d97ade39463a2
hiperion <hiperionr3@telefonica.net>
2015-05-17 22:46:53 +0200

    Section 05 - 084 - Consolidating hit counting

--- Block breaker/Assets/Scripts/Brick.cs
+++ Block breaker/Assets/Scripts/Brick.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class Brick : MonoBehaviour {
 
-	public int maxHits;
+
 	public Sprite[] hitSprites;
+
 
 	private int timesHit;
 	private LevelManager levelManager;
@@ -20,7 +21,15 @@ public class Brick : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D col) {
+		bool isBreakable = (this.tag == "Breakable");
+		if (isBreakable) {
+			HandleHits ();	
+		}
+	}
+
+	void HandleHits(){
 		timesHit++ ;
+		int maxHits = hitSprites.Length + 1;
 		if (timesHit >= maxHits) {
 			Destroy (gameObject);
 		} else {
@@ -30,7 +39,9 @@ public class Brick : MonoBehaviour {
 
 	void LoadSprites() {
 		int spriteIndex = timesHit - 1;
-		this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		if (hitSprites [spriteIndex]) {
+			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		}
 	}
 
 	//TODO Remove this method once we can actually win!
