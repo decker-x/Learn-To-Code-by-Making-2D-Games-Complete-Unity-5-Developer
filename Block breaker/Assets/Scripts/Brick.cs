8cced4f75ed85ed01908e7bdcae83309265bce8a
hiperion <hiperionr3@telefonica.net>
2015-06-05 15:27:38 +0200

    Section 05 - 093 - Using particle systems

--- Block breaker/Assets/Scripts/Brick.cs
+++ Block breaker/Assets/Scripts/Brick.cs
@@ -1,17 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Brick : MonoBehaviour {
+public class Brick : MonoBehaviour
+{
 
 	public AudioClip crack;
 	public Sprite[] hitSprites;
-	public static int breakcableCount=0;
-
+	public static int breakcableCount = 0;
+	public GameObject smoke;
 	private int timesHit;
 	private LevelManager levelManager;
 	private bool isBreakable;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		isBreakable = (this.tag == "Breakable");
 		//Keep track of breakable bricks
 		if (isBreakable) {
@@ -23,45 +25,58 @@ public class Brick : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 
-	void OnCollisionEnter2D(Collision2D col) {
+	void OnCollisionEnter2D (Collision2D col)
+	{
 
 		bool isBreakable = (this.tag == "Breakable");
-		AudioSource.PlayClipAtPoint (crack, transform.position,0.01f);
+		AudioSource.PlayClipAtPoint (crack, transform.position, 0.01f);
 		if (isBreakable) {
 			HandleHits ();	
 		}
 
 	}
 
-	void HandleHits(){
-		timesHit++ ;
+	void HandleHits ()
+	{
+		timesHit++;
 		int maxHits = hitSprites.Length + 1;
 		if (timesHit >= maxHits) {
 			breakcableCount--;
-			Debug.Log(breakcableCount);
+			Debug.Log (breakcableCount);
 			print (breakcableCount);
-			levelManager.BrickDestroyed();
+			levelManager.BrickDestroyed ();
+			PuffSmoke ();
 			Destroy (gameObject);
 		} else {
-			LoadSprites();
+			LoadSprites ();
 		}
 	}
 
-	void LoadSprites() {
+	void PuffSmoke ()
+	{
+		GameObject smokePuff = Instantiate (smoke, gameObject.transform.position, Quaternion.identity) as GameObject;
+		smoke.particleSystem.startColor = gameObject.GetComponent<SpriteRenderer> ().color;
+
+	}
+
+	void LoadSprites ()
+	{
 		int spriteIndex = timesHit - 1;
 		if (hitSprites [spriteIndex] != null) {
 			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
-		}else{
-			Debug.LogError("Brick sprite missing");
+		} else {
+			Debug.LogError ("Brick sprite missing");
 		}
 	}
 
 	//TODO Remove this method once we can actually win!
-	void SimulateWin() {
+	void SimulateWin ()
+	{
 		levelManager.LoadNextLevel ();
 	}
 }
