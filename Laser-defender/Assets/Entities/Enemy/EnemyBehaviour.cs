43eb6bf313fdd4c6b3f0b6c1de38d691968b9886
hiperion <hiperionr3@telefonica.net>
2015-09-22 23:33:15 +0200

    Section 06 - 117 - Sound Effects For Fun And Profit

--- Laser-defender/Assets/Entities/Enemy/EnemyBehaviour.cs
+++ Laser-defender/Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -10,6 +10,9 @@ public class EnemyBehaviour : MonoBehaviour
 	public float shotsPerSeconds = 0.5f;
 	public int scoreValue = 150;
 
+	public AudioClip fireSound;
+	public AudioClip deathSound;
+
 	private ScoreKeeper scoreKeeper;
 
 	void Start(){
@@ -27,9 +30,12 @@ public class EnemyBehaviour : MonoBehaviour
 
 	void Fire ()
 	{
-		Vector3 startPosition = transform.position + new Vector3 (0, -1, 0);
-		GameObject missile = Instantiate (projectile, startPosition, Quaternion.identity) as GameObject;
-		missile.rigidbody2D.velocity = new Vector2 (0, -projectileSpeed);
+//		Vector3 startPosition = transform.position + new Vector3 (0, -1, 0);
+//		GameObject missile = Instantiate (projectile, startPosition, Quaternion.identity) as GameObject;
+//		missile.rigidbody2D.velocity = new Vector2 (0, -projectileSpeed);
+		GameObject laser = Instantiate (projectile, transform.position, Quaternion.identity) as GameObject;
+		laser.rigidbody2D.velocity = new Vector2 (0, -projectileSpeed);
+		AudioSource.PlayClipAtPoint (fireSound, transform.position);
 	}
 
 	void OnTriggerEnter2D (Collider2D collider)
@@ -41,11 +47,16 @@ public class EnemyBehaviour : MonoBehaviour
 			health -= missile.GetDamage ();
 			missile.Hit ();
 			if (health <= 0) {
-				Destroy (gameObject);
-				scoreKeeper.Score(scoreValue);
+				Die ();
 				Debug.Log ("Hit by a projectile");
 			}
 		}
 	}
-	
+
+	void Die(){
+		AudioSource.PlayClipAtPoint (deathSound, transform.position);
+		scoreKeeper.Score(scoreValue);
+		Destroy (gameObject);
+
+	}
 }
