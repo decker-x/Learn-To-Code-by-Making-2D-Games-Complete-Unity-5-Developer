1f69bcb105f1058bd887188dc609257a5800fa93
hiperion <hiperionr3@telefonica.net>
2015-11-06 01:13:50 +0100

    Section 07 - 135 - UI Sliders for options

--- Glitch garden/Assets/Scripts/PlayerPrefsManager.cs
+++ Glitch garden/Assets/Scripts/PlayerPrefsManager.cs
@@ -10,7 +10,7 @@ public class PlayerPrefsManager : MonoBehaviour
 
 	public static void SetMasterVolume (float volume)
 	{
-		if (volume > 0f && volume < 1f) {
+		if (volume >= 0f && volume <= 1f) {
 			PlayerPrefs.SetFloat (MASTER_VOLUME_KEY, volume);
 		} else {
 			Debug.LogError ("Master volume out of range");
@@ -47,16 +47,16 @@ public class PlayerPrefsManager : MonoBehaviour
 
 	public static void setDifficulty (float difficulty)
 	{
-		if (difficulty >= 0f && difficulty <= 1f) {
+		if (difficulty >= 1f && difficulty <= 3f) {
 			PlayerPrefs.SetFloat (DIFFICULTY_KEY, difficulty);
 		} else {
 			Debug.LogError ("Difficulty out of range");
 		}
 	}
 
-	public static float getDifficulty (float difficulty)
+	public static float getDifficulty ()
 	{
-		return PlayerPrefs.GetFloat (DIFFICULTY_KEY, difficulty);
+		return PlayerPrefs.GetFloat (DIFFICULTY_KEY);
 	}
 }
 
